b15140ecf6c47273bbe45d27ddee1b6e5775cfa8
Nadejda1 <pavaruvi@gmail.com>
2015-11-25 18:46:20 +0200

    homework

--- Homework1/Hom2/src/task1.java
+++ Homework1/Hom2/src/task1.java
@@ -3,12 +3,13 @@ public class task1 {
 
 	public static void main(String[] args) {
 		float a = 2.3f;
-		System.out.println();
+		System.out.println(a);
 		float b = 1.6f;	
-		System.out.println();
+		System.out.println(b);
 		float c = a*b;
-		System.out.println();
+		System.out.println(c);
 
 	}
 
 }
+ 
---
+++ Homework1/Hom2/src/task2.java
@@ -0,0 +1,14 @@
+
+public class task2 {
+
+	public static void main(String[] args) {
+		int a=5,b=8,c;
+		System.out.println("a = "+a + ", b = " + b);
+		c=a;
+		a=b;
+		b=c;
+
+		System.out.println("new values: a = "+a + ", b = " + b);
+		
+	}	
+}
